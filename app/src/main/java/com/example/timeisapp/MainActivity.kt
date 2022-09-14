a1d282f9f563b9c80bc4232e54ce4a6b4a7c5830
Kilne <luca.maiuri86@gmail.com>
2022-09-14 16:16:39 +0200

    Test Route

--- app/src/main/java/com/example/timeisapp/MainActivity.kt
+++ app/src/main/java/com/example/timeisapp/MainActivity.kt
@@ -2,7 +2,12 @@ package com.example.timeisapp
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+
+import com.example.timeisapp.database.*
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +18,14 @@ class MainActivity : AppCompatActivity() {
         val registerButton = findViewById<Button>(R.id.Register_button)
 
         loginButton.setOnClickListener {
-            // @TODO: Listener
+            Toast.makeText(this, "Before suspend", Toast.LENGTH_SHORT).show()
+            runBlocking {
+                launch {
+                    Toast.makeText(this@MainActivity, "After suspend", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this@MainActivity,
+                        logMeIn("Luc1","password").toString() , Toast.LENGTH_SHORT).show()
+                }
+            }
         }
 
         registerButton.setOnClickListener {
--- app/src/main/java/com/example/timeisapp/database/Request.kt
+++ app/src/main/java/com/example/timeisapp/database/Request.kt
@@ -1,11 +1,36 @@
 package com.example.timeisapp.database
 
 import io.ktor.client.*
+import io.ktor.client.call.*
 import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
 
-const val BACKEND_URL: String = "http://127.0.0.1:5000"
+const val BACKEND_URL: String = "http://10.0.2.2:5000"
 
-fun buildClient() {
-    val client = HttpClient(CIO)
-    //  @TODO:Finire il client e considerare le route per le request
+fun buildClient() : HttpClient {
+    return HttpClient(CIO){
+        install(UserAgent) {
+            agent = "TimeIsApp Android Client"
+        }
+        install(ContentNegotiation)
+    }
+
 }
+
+suspend fun logMeIn(username: String, password: String) {
+    class LoginResponse(val username: String, val password: String)
+    val client =  buildClient()
+    val response: HttpResponse = client.post("$BACKEND_URL/mobile/login") {
+        contentType(ContentType.Application.Json)
+        setBody(LoginResponse(username, password))
+        }
+    client.close()
+
+    return response.body()
+
+
+    }
